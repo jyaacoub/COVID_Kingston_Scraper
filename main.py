0b1186ce05ac192cebc8ad30c0520bb9aaa251cd
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-03 00:21:54 -0400

    gpio pi test

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from scraper import Bot
 
-bot = Bot()
-bot.requestContent()
-print("Active Cases:", bot.getCases())
-print("Community Status:", bot.getCommunityStatus())
+# bot = Bot()
+# bot.requestContent()
+# print("Active Cases:", bot.getCases())
+# print("Community Status:", bot.getCommunityStatus())
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)

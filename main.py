000876de583ade3844bdd2fa05fe8456b933aa7e
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-03 10:24:28 -0400

    testing rgb

--- main.py
+++ main.py
@@ -10,34 +10,25 @@ from time import sleep
 # red = LED(22)
 # green = LED(27)
 # blue = LED(17)
-PWMLED(17, frequency=100)
-sleep(3)
-PWMLED(17, frequency=50)
-sleep(3)
-PWMLED(17, frequency=25)
-sleep(3)
-PWMLED(17, frequency=10)
-sleep(3)
-PWMLED(17, frequency=1)
 
 
 # Sub in 2 for Red, 3-Orange, 4-Yellow, and 5-Green
-# while True:
-#     # Red
-#     red.on()
-#     sleep(1)
-#
-#     # Orange
-#     # TODO: Figure out how to display orange with rgb led.
-#
-#
-#     # Yellow
-#     green.on()
-#     sleep(1)
-#
-#     # Green
-#     red.off()
-#     sleep(1)
-#
-#     green.off()
+while True:
+    # Red
+    red.on()
+    sleep(1)
+
+    # Orange
+    # TODO: Figure out how to display orange with rgb led.
+
+
+    # Yellow
+    green.on()
+    sleep(1)
+
+    # Green
+    red.off()
+    sleep(1)
+
+    green.off()
 

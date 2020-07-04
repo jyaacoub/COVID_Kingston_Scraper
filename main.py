0f09cbb9b9e1e9b8c259633156083d66cf12a810
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-07-03 23:50:45 -0400

    7-seg debugging (issue with pins/wiring)

--- main.py
+++ main.py
@@ -76,15 +76,34 @@ digits = (22, 27, 17, 24)
 
 for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
-    GPIO.output(digit, 0)
+    GPIO.output(digit, 1)
+
+num = {' ': (0, 0, 0, 0, 0, 0, 0),
+       '0': (1, 1, 1, 1, 1, 1, 0),
+       '1': (0, 1, 1, 0, 0, 0, 0),
+       '2': (1, 1, 0, 1, 1, 0, 1),
+       '3': (1, 1, 1, 1, 0, 0, 1),
+       '4': (0, 1, 1, 0, 0, 1, 1),
+       '5': (1, 0, 1, 1, 0, 1, 1),
+       '6': (1, 0, 1, 1, 1, 1, 1),
+       '7': (1, 1, 1, 0, 0, 0, 0),
+       '8': (1, 1, 1, 1, 1, 1, 1),
+       '9': (1, 1, 1, 1, 0, 1, 1)}
 
 try:
     while True:
-        for digit in digits:
-            GPIO.output(digit, 0)
-            time.sleep(0.001)
-            GPIO.output(digit, 1)
-
-
+        n = time.ctime()[11:13] + time.ctime()[14:16]
+        s = str(n).rjust(4)
+        for digit in range(4):
+            for loop in range(0, 7):
+                GPIO.output(segments[loop], num[s[digit]][loop])
+                if (int(time.ctime()[18:19]) % 2 == 0) and (digit == 1):
+                    GPIO.output(25, 1)
+                else:
+                    GPIO.output(25, 0)
+            GPIO.output(digits[digit], 0)
+            time.sleep(1)
+            GPIO.output(digits[digit], 1)
 finally:
     GPIO.cleanup()
+
